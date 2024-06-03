12ad7f600a088e49083a62bfb69670ef93aa11f1
Juri Sajzew <j.sajzew@outlook.de>
2024-06-03 12:26:25 +0200

    created user and app component

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { MatTooltipModule } from '@angular/material/tooltip';
-import { MatDialog } from '@angular/material/dialog';
+import { MatDialog, MatDialogModule } from '@angular/material/dialog';
 import { DialogAddUserComponent } from '../dialog-add-user/dialog-add-user.component';
 import { User } from '../../models/user.class';
 @Component({
@@ -12,6 +12,7 @@ import { User } from '../../models/user.class';
     MatButtonModule,
     MatIconModule,
     MatTooltipModule,
+    MatDialogModule,
   ],
   templateUrl: './user.component.html',
   styleUrl: './user.component.scss'
