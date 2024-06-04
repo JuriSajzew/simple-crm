82f028a06942c58b6a0e132dd77a4dff54d436d9
Juri Sajzew <j.sajzew@outlook.de>
2024-06-04 15:53:31 +0200

    user lists created and designed with scss.

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -5,6 +5,7 @@ import { MatTooltipModule } from '@angular/material/tooltip';
 import { MatDialog, MatDialogModule } from '@angular/material/dialog';
 import { DialogAddUserComponent } from '../dialog-add-user/dialog-add-user.component';
 import { User } from '../../models/user.class';
+import {MatCardModule} from '@angular/material/card';
 @Component({
   selector: 'app-user',
   standalone: true,
@@ -13,6 +14,7 @@ import { User } from '../../models/user.class';
     MatIconModule,
     MatTooltipModule,
     MatDialogModule,
+    MatCardModule,
   ],
   templateUrl: './user.component.html',
   styleUrl: './user.component.scss'
