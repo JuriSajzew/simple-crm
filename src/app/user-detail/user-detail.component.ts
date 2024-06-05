6b5b1c6334bf94339da75406d2ea51c2918e6f01
Juri Sajzew <j.sajzew@outlook.de>
2024-06-05 17:00:25 +0200

    user-detail- card created Query from Firebase DB inserted into the .ts file

--- src/app/user-detail/user-detail.component.ts
+++ src/app/user-detail/user-detail.component.ts
@@ -1,15 +1,49 @@
-import { Component } from '@angular/core';
-import {MatCardModule} from '@angular/material/card';
+import { Component, OnInit } from '@angular/core';
+import { Firestore, collection, addDoc, docData, doc } from '@angular/fire/firestore';
+import { MatCardModule } from '@angular/material/card';
+import { ActivatedRoute } from '@angular/router';
+import { User } from '../../models/user.class';
+import { MatIconModule } from '@angular/material/icon';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-user-detail',
   standalone: true,
   imports: [
     MatCardModule,
+    MatIconModule,
+    MatDividerModule,
+    MatButtonModule,
   ],
   templateUrl: './user-detail.component.html',
   styleUrl: './user-detail.component.scss'
 })
-export class UserDetailComponent {
+export class UserDetailComponent implements OnInit {
+  userId = '';
+  user: User = new User();
 
+  constructor(private route: ActivatedRoute, private firestore: Firestore) { }
+
+
+  ngOnInit(): void {
+    this.route.paramMap.subscribe(paramMap => {
+      const id = paramMap.get('id');
+      this.userId = id !== null ? id : '';
+      console.log('GO ID ', this.userId);
+      this.getUser();
+    })
+  }
+
+  async getUser() {
+    const userDocRef = doc(this.firestore, `users/${this.userId}`);
+    docData(userDocRef).subscribe((user: any) => {
+      this.user = new User(user);
+      console.log('Retrieved user ', this.user);
+    })
+  }
+
+  openAddressDialog(){
+    
+  }
 }
